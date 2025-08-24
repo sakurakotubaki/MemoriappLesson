b91673e42ea0c56ebd4a9157adda734f5ff53eeb
sakurakotubaki <gitonly543@gmail.com>
2025-08-24 23:46:38 +0900

    Initial Commit

---
+++ MemoriappLesson/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  MemoriappLesson
+//
+//  Created by 橋本純一 on 2025/08/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
